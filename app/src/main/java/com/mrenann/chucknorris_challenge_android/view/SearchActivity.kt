46257f3ec32eec963f7a1b6bc41c092012517af5
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-26 17:47:41 -0300

    Minor changes in SearchActivity.kt

--- app/src/main/java/com/mrenann/chucknorris_challenge_android/view/SearchActivity.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/view/SearchActivity.kt
@@ -3,10 +3,15 @@ package com.mrenann.chucknorris_challenge_android.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mrenann.chucknorris_challenge_android.R
+import com.mrenann.chucknorris_challenge_android.databinding.ActivityMainBinding
+import com.mrenann.chucknorris_challenge_android.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
