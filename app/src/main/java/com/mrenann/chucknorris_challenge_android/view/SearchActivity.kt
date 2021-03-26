f1bbbf064434d828a5603a77590a36e6d5ec0656
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-26 01:12:39 -0300

    Create SearchActivity.kt

---
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/view/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.mrenann.chucknorris_challenge_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrenann.chucknorris_challenge_android.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
