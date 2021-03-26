791ebb0cfa5046c6448c72f52def6e6ad336795f
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-26 01:24:02 -0300

    Change Models Facts

--- app/src/main/java/com/mrenann/chucknorris_challenge_android/model/FactsResult.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/FactsResult.kt
@@ -1,6 +1,6 @@
 package com.mrenann.chucknorris_challenge_android.model
 
 data class FactsResult(
-    var result: List<Result>?,
+    var result: List<Fact>?,
     var total: Int?
 )
--- app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
@@ -1,6 +1,6 @@
 package com.mrenann.chucknorris_challenge_android.model
 
-data class Result(
+data class Fact(
     var categories: List<String>?,
     var created_at: String?,
     var icon_url: String?,
