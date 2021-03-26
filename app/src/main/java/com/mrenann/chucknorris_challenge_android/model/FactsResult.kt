be54bd9baaeeb891a7042755bb5ba4ae299f3614
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-26 01:22:38 -0300

    Create Models from Facts result

---
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/FactsResult.kt
@@ -0,0 +1,6 @@
+package com.mrenann.chucknorris_challenge_android.model
+
+data class FactsResult(
+    var result: List<Result>?,
+    var total: Int?
+)
---
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
@@ -0,0 +1,11 @@
+package com.mrenann.chucknorris_challenge_android.model
+
+data class Result(
+    var categories: List<String>?,
+    var created_at: String?,
+    var icon_url: String?,
+    var id: String?,
+    var updated_at: String?,
+    var url: String?,
+    var value: String?
+)
