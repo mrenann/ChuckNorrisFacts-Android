cd8c57b0d30b73d873ba76e9de05f9b52a7bf6e9
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-28 22:01:31 -0300

    Remove some Warnings

--- app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
@@ -1,7 +1,7 @@
 package com.mrenann.chucknorris_challenge_android.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Fact(
