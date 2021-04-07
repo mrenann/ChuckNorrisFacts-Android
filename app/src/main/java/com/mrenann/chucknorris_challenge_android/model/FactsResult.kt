f50343af5ff406f45804532b327b6fa22bc22d41
Marcos Renann <marcos.renann.br@gmail.com>
2021-04-07 18:45:37 -0300

    Remove Parcelize

--- app/src/main/java/com/mrenann/chucknorris_challenge_android/model/FactsResult.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/FactsResult.kt
@@ -1,10 +1,6 @@
 package com.mrenann.chucknorris_challenge_android.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class FactsResult(
     var result: MutableList<Fact>?,
     var total: Int?
-):Parcelable
+)
--- app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/model/Result.kt
@@ -1,9 +1,5 @@
 package com.mrenann.chucknorris_challenge_android.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Fact(
     var categories: MutableList<String>?,
     var created_at: String?,
@@ -12,4 +8,4 @@ data class Fact(
     var updated_at: String?,
     var url: String?,
     var value: String?
-): Parcelable
+)
