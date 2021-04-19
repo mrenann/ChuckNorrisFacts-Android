ad0684abe0233ecb469c4d2d5b4d8df373012fe9
Marcos Renann <marcos.renann.br@gmail.com>
2021-04-19 20:44:38 -0300

    Unused Dependencies Removed

--- app/src/main/java/com/mrenann/chucknorris_challenge_android/view/adapters/FactsAdapter.kt
+++ app/src/main/java/com/mrenann/chucknorris_challenge_android/view/adapters/FactsAdapter.kt
@@ -1,6 +1,5 @@
 package com.mrenann.chucknorris_challenge_android.view.adapters
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
